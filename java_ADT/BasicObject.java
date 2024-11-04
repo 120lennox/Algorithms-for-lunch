2ba2e49e45940f41a401d89c86b13ce9e0103682
Lennox Mountain <lennoxmountain67@gmail.com>
2024-11-04 13:13:16 +0200

    learning about java interfaces

---
+++ java_ADT/BasicObject.java
@@ -0,0 +1,9 @@
+package java_ADT;
+
+public  class BasicObject{
+    private Object obj;
+
+    Object get(){ return obj; }
+    void set(Object obj){ this.obj = obj; }
+}
+
---
+++ java_ADT/Generics.java
@@ -0,0 +1,4 @@
+import java_ADT.BasicObject;
+
+
+
