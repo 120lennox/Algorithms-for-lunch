54ff811fadc41ba119ae12ecce97b1c52c2d3422
Lennox Mountain <lennoxmountain67@gmail.com>
2024-12-15 23:43:39 +0200

    binary search working successfully

--- algorithm_analysis/BinarySearch.cpp
+++ algorithm_analysis/BinarySearch.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int key){
+int binarySearch(int arr[], int key, int length){
     int left = 0;
-    int length = sizeof(arr) / sizeof(arr[0])
     int right = length - 1;
 
-    while (left <= right){
-        int mid = (left + (right - left)) / 2;
+    while (left != right){
+        int mid = (left + right) / 2;
 
         if (arr[mid] == key){
             return mid;
         }
 
-        if (arr[mid] < key){
-            left = mid - 1;
+        if (key > arr[mid]){
+            left = mid + 1;
         }
 
         else{
-            right = mid + 1;
+            right = mid - 1;
         }
-
-        return -1;
     }
+
+    return -1;
 }
 
 int main(){
     int arr[] = {1, 3, 4, 5, 7, 8, 21};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    cout<<"size = "<<length<<endl;
     int key = 8;
 
-    int result = binarySearch(arr, key);
+    int result = binarySearch(arr, key, length);
 
     if (result == -1){
         printf("key not found!\n");
