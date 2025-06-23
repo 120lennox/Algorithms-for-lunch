585775e4c644df273255f73bb75fd502e5509107
Lennox Mountain <lennoxmountain67@gmail.com>
2025-06-23 11:58:43 +0200

    added a Link node class

---
+++ List/Link.java
@@ -0,0 +1,31 @@
+public class Link <E> {
+    private E element; // value in a linked list
+    private Link <E> next; // reference to the next node in the list
+
+    //constractors
+    Link(E item, Link<E> nextval ){
+        element = item;
+        next = nextval;
+    }
+
+    Link (Link<E> nextval){
+        next = nextval;
+    }
+
+    Link <E> next(){
+        return next;
+    }
+
+    Link <E> setNext(Link<E> nextval){
+       return next = nextval;
+    }
+
+    E element(){
+        return element;
+    }
+
+    E setElement(E item){
+        return element = item;
+    }
+
+}
