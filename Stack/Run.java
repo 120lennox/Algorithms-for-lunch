de659bbc857a28ebc2d1336a2d61ca5a658aaa6e
Lennox Mountain <lennoxmountain67@gmail.com>
2025-06-24 21:06:25 +0200

    added queue data structure

--- Stack/Run.java
+++ Stack/Run.java
@@ -18,7 +18,7 @@ public class Run {
         // printing top value of the stack
         System.out.println("Top value of the stack is " + stack.topValue());
 
-        printing all elements of the stack
+        // printing all elements of the stack
         while (stack.length() != 0){
             System.out.println();
             System.out.println("Element on position " + (stack.length() - 1) + " is " + stack.pop());
