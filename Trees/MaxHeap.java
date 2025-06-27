aab2b4565785ad811ddd70fbd8ff1e6133257cc1
Lennox Mountain <lennoxmountain67@gmail.com>
2025-06-27 23:13:08 +0200

    `Added instance variables to track heap size and item count in MaxHeap class.`

--- Trees/MaxHeap.java
+++ Trees/MaxHeap.java
@@ -1,5 +1,9 @@
 package Trees;
 
 public class MaxHeap<E extends Comparable<? super E>> {
+    private E[] heap; // pointer to heap array
+    private int size; // maximum size of the heap
+    private int count; // number of items in the heap
+
     
 }
