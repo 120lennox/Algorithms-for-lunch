38cfc2cf673b1f16e7ed1c55bf4119634b960047
Lennox Mountain <lennoxmountain67@gmail.com>
2025-06-28 12:27:20 +0200

    building a new maxHeap structure

--- Trees/MaxHeap.java
+++ Trees/MaxHeap.java
@@ -5,5 +5,22 @@ public class MaxHeap<E extends Comparable<? super E>> {
     private int size; // maximum size of the heap
     private int count; // number of items in the heap
 
+    // constructor for pre loading heap elements
+    public MaxHeap(E[] pointer, int size, int count) {
+        heap = pointer;
+        this.size = size;
+        this.count = count;
+        buildheap();
+    }
+
+    public int heapSize(){
+        return count;
+    }
+
+    public boolean isLeaf(int pos ){
+        return (pos >= count/2) && (pos < count);
+    }
+
     
+
 }
