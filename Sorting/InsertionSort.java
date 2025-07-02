a59b03aa9ca5cb51e80736f3c1bb529ac37cb3c6
Lennox Mountain <lennoxmountain67@gmail.com>
2025-07-02 21:50:28 +0200

    `Added swap method and implementation of insertion sort algorithm in InsertionSort.java`

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,5 +1,14 @@
 package Sorting;
 
 public class InsertionSort {
-    
+    public void swap()
+    static <E extends Comparable <? super E>>
+    void insertionSort(E[] arr) {
+        int length = arr.length;
+        for (int i = 1; i < length; i++) {
+            for (int j = i; (j > 0) && (arr[j].compareTo(arr[j - 1]) < 0); j--) {
+                
+            }
+        }
+   }
 }
