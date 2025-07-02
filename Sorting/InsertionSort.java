838bd060efc1398093c885c16819c8838280aff8
Lennox Mountain <lennoxmountain67@gmail.com>
2025-07-02 21:56:05 +0200

    implemented swap and insertion sort algorithm

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -7,13 +7,13 @@ public class InsertionSort {
         array[i] = array[j];
         array[j] = temp;
     }
-    
+
     static <E extends Comparable <? super E>>
     void insertionSort(E[] arr) {
         int length = arr.length;
         for (int i = 1; i < length; i++) {
             for (int j = i; (j > 0) && (arr[j].compareTo(arr[j - 1]) < 0); j--) {
-                
+                swap(j, j-1, arr);
             }
         }
    }
